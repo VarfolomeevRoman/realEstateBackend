7b448881a1bc61cd11ab68c8e6b8c9c90cb30101
Roman <102508118+VarfolomeevRoman@users.noreply.github.com>
2023-10-12 10:37:52 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+mongoose
+    .connect(
+        'mongodb+srv://Admin:Qwerty@clusterrealestate0.oojagvb.mongodb.net/?retryWrites=true&w=majority'
+    )
+    .then(() => console.log('DB okay'))
+    .catch((err) => console.log('DB error', err));
+
+const app = express();
+app.use(express.json());
+const port = 4444;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world');
+});
+
+app.post('/auth/login', (req: Request, res: Response) => {
+    console.log(req.body);
+
+    const token = jwt.sign(
+        {
+            email: req.body.email,
+            fullName: 'Ivan Ivanov',
+        },
+        'secret123 '
+    );
+
+    res.json({
+        success: true,
+        token,
+    });
+});
+app.listen(port, (err?: Error) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Server okay');
+});
